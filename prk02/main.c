79ff5857a6b6ebf57c70ae11be58e53537e45073
Christian Vervoorts <cvervoorts@googlemail.com>
2010-05-08 14:56:07 +0200

    prk02: Hinzufuegen der usage-meldung

--- prk02/main.c
+++ prk02/main.c
@@ -7,8 +7,21 @@
 #include <stdio.h>
 #include "db.h"
 
+void print_usage() {
+    printf("Usage:\n");
+    printf("\t dbimp [options] <infile>\n");
+    printf("Options:\n");
+    printf("\t-del delete table contents before import\n");
+    printf("\t-u database user\n");
+    printf("\t-p password\n");
+    printf("\t-h database host\n");
+    printf("\t-d database name\n");
+}
+
 int main(int argc, char** argv)
 {
-    
+    if(argc <= 1){
+        print_usage();
+    }
     return 0;
 }
